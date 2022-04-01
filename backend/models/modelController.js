ed626d91e8e8debe90ef58aaa4031fffd96af90c
seongwon-gitcodes <ysw991106@gmail.com>
2022-04-02 00:56:26 +0900

    update db set

--- backend/models/modelController.js
+++ backend/models/modelController.js
@@ -1,9 +1,12 @@
-// controlDb
+// modelController.js
 
+// 사용할 DB
 var mysql = require('mysql2');
 
+// DB config
 var config = require('./config');
 
+// connect db
 exports.init = function init() {
     var conn = mysql.createConnection(config, (err) => { if (err) throw err});
     conn.connect((err) => {if (err) throw err});
