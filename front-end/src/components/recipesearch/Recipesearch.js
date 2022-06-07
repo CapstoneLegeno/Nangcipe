4da4709b09182f93c6ba873d8783eeafd6846489
lee-hyunsu <dlgustn675@naver.com>
2022-06-08 05:38:54 +0900

    recipesearch-add

--- front-end/src/components/recipesearch/Recipesearch.js
+++ front-end/src/components/recipesearch/Recipesearch.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect,  } from 'react';
 
-const elementstyle = styled.div`
+// import styled from 'styled-components';
+import axios from 'axios';
+
+// const elementstyle = styled.div`
+
+// `
+const RecipeList = () => {
+    const [search, setSearch] = useState("");
+    const [lists, setLists] = useState([]);
+    // const router = useRouter();
+
+    useEffect(() => {
+        const recipeData = async () => {
+            await axios.post("http://localhost:3001/recipe/search", {
+                ingrdients: ""
+            }).then((res) => {
+                console.log(res)
+            }).catch((err) => {;
+                console.log(err);
+            })
+        };
+        recipeData();
+    }, [])
+
+    const onChangeSearch = (e) => {
+        e.preventDefault();
+        setSearch(e.target.value);
+    };
     
-`
-
-function Recipesearch(){
+    const onSearch = (e) => {
+        e.preventDefault();
+        if(search === null || search === '') {
+            axios.post("http://localhost:3001/recipe/search", {
+                ingrdients: ""
+            }).then((res) => {
+                console.log(res)
+            }).catch((err) => {;
+                console.log(err);
+            })
+        } else {
+            const filterData = lists.filter((row) => row.recipe.include)
+        }
+    }
+    
+    
     return(
         <div className='recipesearch'>
-            <input type={"text"}
-                    className={"input-sm"}
-                    size={"30"}> 
-            </input>
+            <form onSubmit={e => onSearch(e)}>
+                <input type={"text"}
+                        className={"input-sm"}
+                        size={"30"}
+                        value={search}
+                        placeholder="조회할 레시피를 입력하세요."
+                        onChange={onChangeSearch} /> 
+                <button type='submit'>조회</button>
+                    
+            </form>
         </div>
     )
+    
+
 }
 
-export default Recipesearch;
+
+
+
+
+export default RecipeList;
